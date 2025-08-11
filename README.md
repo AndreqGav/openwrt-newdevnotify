aa0cb1b4184a8750cd849f227901395f45788b44
Андрей <andreq.gav@mail.ru>
2025-08-11 14:00:27 +0300

    Добавлено шаблон сообщения и отображение базы клиентов в luci Получение db_path из конфигурации. Обновлено README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # openwrt-newdevnotify
 Sends Telegram message when a new DHCP client appears; deduplicates by MAC and NETWORK NAME.
+
+Для настройки необходимо создать своего бота в телеграмм и указать его токен и идентификатор чат с вами в конфигурации:
+
+1. Создайте бота в телеграмме через @BotFather
+
+2. Начните диалог с ботом
+
+3. Узнайте идентификатор чата (в личном сообщение chat id = ваш идентификатор)
+
+	Можно узанть отправив запрос GET https://api.telegram.org/bot{{notify_bot_token}}/getUpdates и в нем будет chat.id с вами
+
+4. Установить параметры token, chat_id в /etc/config/newdevnotify или используйте плагин luci-app-newdevnotify и через LuCI сделайте это (Services->New Device Notify)
--- package/luci-app-newdevnotify/luasrc/model/cbi/newdevnotify.lua
+++ package/luci-app-newdevnotify/luasrc/model/cbi/newdevnotify.lua
@@ -1,3 +1,6 @@
+local uci = require "luci.model.uci".cursor()
+local db_path = uci:get("newdevnotify", "main", "db_path") or ""
+
 m = Map("newdevnotify", translate("New Device Notify"),
     translate("Send Telegram alerts when a new device connects to the network."))
 
@@ -15,10 +18,41 @@ chat_id.datatype = "string"
 main_label = s:option(Value, "main_label", translate("Main network label"))
 guest_label = s:option(Value, "guest_label", translate("Guest network label"))
 
+template = s:option(TextValue, "template", translate("Message template for notification"))
+template.rows = 8
+template.wrap = "off"
+template.rmempty = true
+template.description = translate("Use variables: $NETNAME, $DATE, $HOST, $MAC, $IP.")
+
+function template.cfgvalue(self, section)
+    local val = self.map.uci:get("newdevnotify", section, "template")
+    if val then
+        val = val:gsub("\\n", "\n")
+    end
+    return val
+end
+
+function template.write(self, section, value)
+    if value then
+        value = value:gsub("\r\n?", "\n")
+        value = value:gsub("\n", "\\n")
+    end
+    self.map.uci:set("newdevnotify", section, "template", value)
+end
+
 btn = s:option(Button, "_reset", translate("Forget all devices"))
 btn.inputstyle = "reset"
 function btn.write()
-    nixio.fs.writefile("/etc/newdevnotify/known_clients.db", "")
+    nixio.fs.writefile(db_path, "")
+end
+
+db = s:option(TextValue, "_known_clients", translate("Known clients database"))
+db.rows = 15
+db.wrap = "off"
+db.readonly = true
+
+function db.cfgvalue(self, section)
+    return nixio.fs.readfile(db_path) or ""
 end
 
 return m
